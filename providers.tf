db27455fbf76097a3fd2982cba94985f88775bba
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 00:46:06 +0200

    provider

--- providers.tf
+++ providers.tf
@@ -3,17 +3,13 @@ provider "hcloud" {
 
 provider "rancher2" {
   alias = "bootstrap"
-
   api_url   = "https://rancher.${local.domain}"
   bootstrap = true
 }
 
 provider "rancher2" {
-  alias = "admin"
-
   api_url   = "https://rancher.${local.domain}"
   token_key = rancher2_bootstrap.admin.token
-  insecure  = true
 }
 
 provider "rke" {
