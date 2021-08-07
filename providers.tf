e0f7ab071e1f05f60ee30d856cef77ba3b1a315b
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-07 17:42:04 +0200

    fix providers

--- providers.tf
+++ providers.tf
@@ -2,6 +2,14 @@ provider "hcloud" {
   token = var.hcloud_token
 }
 
+provider "rancher2" {
+  alias = "admin"
+
+  api_url = rancher2_bootstrap.admin.url
+  token_key = rancher2_bootstrap.admin.token
+  insecure = true
+}
+
 provider "rke" {
 }
 
@@ -9,17 +17,4 @@ provider "helm" {
   kubernetes {
     config_path = local_file.kube_cluster_yaml.filename
   }
-}
-
-provider "rancher2" {
-  version = "1.16.0"
-  alias     = "bootstrap"
-  api_url   = "https://${local.name}.${local.domain}"
-  bootstrap = true
-}
-
-provider "rancher2" {
-  version = "1.16.0"
-  api_url   = "https://${local.name}.${local.domain}"
-  token_key = rancher2_bootstrap.admin.token
 }
--- versions.tf
+++ versions.tf
@@ -4,5 +4,16 @@ terraform {
       source  = "hetznercloud/hcloud"
       version = "1.28.1"
     }
+    rancher2 = {
+      source = "rancher/rancher2"
+      version = "1.16.0"
+    }
+    rke = {
+      source = "rancher/rke"
+      version = "1.2.3"
+    }
+    helm = {
+      version ="2.2.0"
+    }
   }
 }
