1340238565fde4bed90541a7c89a2aff35ef1b8c
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 17:02:42 +0200

    format

--- providers.tf
+++ providers.tf
@@ -12,9 +12,10 @@ provider "rancher2" {
 }
 
 provider "rancher2" {
+  alias     = "admin"
   api_url   = "https://${local.rancher_hostname}.${local.domain}:8443"
   token_key = rancher2_bootstrap.admin.token
-  insecure = true
+  insecure  = true
 }
 
 provider "rke" {
