160d5e0ad0eed801c4582cc0b0f3c783af7c6139
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 14:49:16 +0200

    no helm

--- rancher.tf
+++ rancher.tf
@@ -1,15 +1,15 @@
 # install rancher
-resource "helm_release" "rancher" {
-  name = "rancher"
-  #  repository = "https://releases.rancher.com/server-charts/latest"
-  chart     = "rancher-latest/rancher"
-  version   = var.rancher_version
-  namespace = "cattle-system"
-  set {
-    name  = "hostname"
-    value = "${local.rancher_hostname}.${local.domain}"
-  }
-}
+# resource "helm_release" "rancher" {
+#   name = "rancher"
+#   #  repository = "https://releases.rancher.com/server-charts/latest"
+#   chart     = "rancher-latest/rancher"
+#   version   = var.rancher_version
+#   namespace = "cattle-system"
+#   set {
+#     name  = "hostname"
+#     value = "${local.rancher_hostname}.${local.domain}"
+#   }
+# }
 
 resource "null_resource" "wait_for_rancher" {
   provisioner "local-exec" {
@@ -33,7 +33,7 @@ EOF
 
     environment = {
       RANCHER_HOSTNAME = "${local.rancher_hostname}.${local.domain}"
-      TF_LINK          = helm_release.rancher.name
+      # TF_LINK          = helm_release.rancher.name
     }
   }
 }
