1094dae1000dce2b8d884c87404098ca02948c3b
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 15:51:31 +0200

    remove resource

--- dns.tf
+++ dns.tf
@@ -2,10 +2,6 @@ data "hetznerdns_zone" "dns_zone" {
   name = local.domain
 }
 
-data "hcloud_server" "web" {
-  name = local.rancher_hostname
-}
-
 resource "hetznerdns_record" "rancher" {
   zone_id = data.hetznerdns_zone.dns_zone.id
   name    = local.rancher_hostname
