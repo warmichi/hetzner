5d855b7d66c1576acd2c5c994dd76530b0fb95e0
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 03:05:14 +0200

    network

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "hcloud_server" "rancher" {
 }
 
 resource "hcloud_server_network" "srvnetwork" {
-  server_id  = hcloud_server.rancher[1]
+  server_id  = hcloud_server.rancher[0]
   network_id = hcloud_network.net.id
 }
 
