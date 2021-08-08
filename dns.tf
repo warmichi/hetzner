822f3ee773db3ab44761a9ee3074cd752cbcbf81
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 15:18:43 +0200

    format

--- dns.tf
+++ dns.tf
@@ -8,8 +8,8 @@ data "hcloud_server" "web" {
 
 resource "hetznerdns_record" "web" {
     zone_id = data.hetznerdns_zone.dns_zone.id
-    name = "www"
-    value = hcloud_server.rancher[0].ipv4_address
-    type = "A"
-    ttl= 60
+    name    = "www"
+    value   = hcloud_server.rancher[0].ipv4_address
+    type    = "A"
+    ttl     = 60
 }
