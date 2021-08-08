45c198973138b1522844ae2dad9a4a3b9b7d0ba0
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 17:48:36 +0200

    bootstrap insecure

--- providers.tf
+++ providers.tf
@@ -9,6 +9,7 @@ provider "rancher2" {
   alias     = "bootstrap"
   api_url   = "https://${local.rancher_hostname}.${local.domain}:8443"
   bootstrap = true
+  insecure  = true
 }
 
 provider "rancher2" {
