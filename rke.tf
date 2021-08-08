2da1191d6303acea1556686af389bea28966e25b
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-08 13:15:47 +0200

    output file conent

--- rke.tf
+++ rke.tf
@@ -11,7 +11,7 @@ resource "rke_cluster" "rancher_server" {
       internal_address = nodes.value.ipv4_address
       user             = "root"
       role             = ["controlplane", "etcd", "worker"]
-      ssh_key          = var.HCLOUD_SSH_RANCHER_PRIVATE_KEY
+      ssh_key          = file(var.HCLOUD_SSH_RANCHER_PRIVATE_KEY)
     }
   }
 
