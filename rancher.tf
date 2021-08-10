f844f619caee571365cf4b064d5f4941f21ddd90
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-10 20:40:29 +0000

    remove some lines

--- rancher.tf
+++ rancher.tf
@@ -1,16 +1,3 @@
-# install rancher
-# resource "helm_release" "rancher" {
-#   name = "rancher"
-#   #  repository = "https://releases.rancher.com/server-charts/latest"
-#   chart     = "rancher-latest/rancher"
-#   version   = var.rancher_version
-#   namespace = "cattle-system"
-#   set {
-#     name  = "hostname"
-#     value = "${local.rancher_hostname}.${local.domain}"
-#   }
-# }
-
 resource "null_resource" "wait_for_rancher" {
   provisioner "local-exec" {
     command = <<EOF
@@ -26,17 +13,14 @@ EOF
 
     environment = {
       RANCHER_HOSTNAME = "${local.rancher_hostname}.${local.domain}"
-      # TF_LINK          = helm_release.rancher.name
     }
   }
   depends_on = [
-    hetznerdns_record.rancher,
+    hetznerdns_record.rancher
   ]
 }
 
 resource "rancher2_bootstrap" "admin" {
-  # provider = rancher2.bootstrap
-
   depends_on = [null_resource.wait_for_rancher]
   password   = "test"
 }
