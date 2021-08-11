a67761de5501d8ec5416f10f29836d37102af586
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-11 20:43:47 +0000

    fix broken variables

--- data.tf
+++ data.tf
@@ -1,16 +1,6 @@
-# resource "helm_release" "rancher_stable" {
-#   name  = "rancher-stable"
-#   chart = "rancher/rancher2"
-# }
-
 data "template_file" "cloud_init" {
   template = file("${path.module}/files/cloud-config.yaml")
   vars = {
-    rancher_token_key = local.RANCHER_TOKEN_KEY
+    RANCHER_TOKEN_KEY = var.RANCHER_TOKEN_KEY
   }
-}
-
-data "rancher2_user" "admin" {
-  username   = "admin"
-  depends_on = [rancher2_bootstrap.admin]
 }
--- locals.tf
+++ locals.tf
@@ -4,7 +4,6 @@ resource "random_id" "id" {
 
 locals {
   rancher_hostname   = "${var.cluster_name}-${random_id.id.hex}"
-  rancher_token      = var.RANCHER_TOKEN_KEY
   rancher_node_count = 1
   rancher_api_url    = "https://${local.rancher_hostname}.${local.domain}:8443"
   domain             = "uwannah.com"
