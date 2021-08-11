89028779091704aeb7ac4b3f7ac7b899d20a8bda
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-11 20:59:47 +0000

    rewrite line

--- main.tf
+++ main.tf
@@ -32,35 +32,3 @@ resource "hcloud_server_network" "srvnetwork" {
   server_id  = hcloud_server.rancher[0].id
   network_id = hcloud_network.net.id
 }
-
-
-########################################
-### Wait for docker install on nodes
-########################################
-# resource "null_resource" "wait_for_docker" {
-#   count = local.rancher_node_count
-
-#   triggers = {
-#     instance_ids = join(",", concat(hcloud_server.rancher.*.id))
-#   }
-
-#   provisioner "local-exec" {
-#     command = <<EOF
-# while [ "$${RET}" -gt 0 ]; do
-#     ssh -q -o StrictHostKeyChecking=no -i $${KEY} $${USER}@$${IP} 'docker ps 2>&1 >/dev/null'
-#     RET=$?
-#     if [ "$${RET}" -gt 0 ]; then
-#         sleep 10
-#     fi
-# done
-# EOF
-
-
-#     environment = {
-#       RET  = "1"
-#       USER = "root"
-#       IP   = element(concat(hcloud_server.rancher.*.ipv4_address), count.index)
-#       KEY  = "${var.HCLOUD_SSH_RANCHER_PRIVATE_KEY}"
-#     }
-#   }
-# }
