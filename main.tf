a7c9456c4e8a339ff84ac75339297ab86350bb45
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-14 01:44:25 +0000

    remove rancher file

--- main.tf
+++ main.tf
@@ -40,3 +40,25 @@ resource "hcloud_server" "rancher" {
     hcloud_network_subnet.network_subnet
   ]
 }
+
+resource "null_resource" "wait_for_rancher" {
+  provisioner "local-exec" {
+    command = <<EOF
+while [ "$${resp}" != "pong" ]; do
+    resp=$(curl -sSk -m 2 "https://$${RANCHER_HOSTNAME}:8443/ping")
+    echo "Rancher Response: $${resp}"
+    if [ "$${resp}" != "pong" ]; then
+      sleep 10
+    fi
+done
+EOF
+
+
+    environment = {
+      RANCHER_HOSTNAME = "${local.rancher_hostname}.${local.domain}"
+    }
+  }
+  depends_on = [
+    hetznerdns_record.rancher
+  ]
+}
