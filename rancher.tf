01a7004f85b1c3fce45b6731679ecffaba55f743
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-14 11:22:37 +0000

    token

--- rancher.tf
+++ rancher.tf
@@ -24,6 +24,5 @@ resource "rancher2_bootstrap" "admin" {
   provider   = rancher2.bootstrap
   depends_on = [null_resource.wait_for_rancher]
   password   = var.RANCHER_UI_PASSWORD
-  token      = var.RANCHER_TOKEN_KEY
 }
 
