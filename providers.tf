0c10ed6d9f4f71fa6f8cba23c38c06c1655776fb
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-14 12:43:02 +0200

    bootstrap

--- providers.tf
+++ providers.tf
@@ -10,8 +10,9 @@ provider "rancher2" {
   insecure  = true
 }
 
-# provider "rancher2" {
-#   api_url   = rancher2_bootstrap.admin.url
-#   token_key = rancher2_bootstrap.admin.token
-#   insecure  = true
-# }
+provider "rancher2" {
+  alias     = "admin"  
+  api_url   = rancher2_bootstrap.admin.url
+  token_key = rancher2_bootstrap.admin.token
+  insecure  = true
+}
