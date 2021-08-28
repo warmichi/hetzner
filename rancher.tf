6c5ad43c2844ff858a078d0826e2a2169c950a69
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-28 02:02:04 +0200

    ansible

--- rancher.tf
+++ rancher.tf
@@ -1,24 +1,34 @@
-resource "null_resource" "wait_for_rancher" {
-  provisioner "local-exec" {
-    command = <<EOF
-while [ "$${resp}" != "pong" ]; do
-    resp=$(curl -sSk -m 2 "https://$${RANCHER_HOSTNAME}:8443/ping")
-    echo "Rancher Response: $${resp}"
-    if [ "$${resp}" != "pong" ]; then
-      sleep 10
-    fi
-done
-EOF
-
-
-    environment = {
-      RANCHER_HOSTNAME = "${local.rancher_hostname}.${local.domain}"
-    }
+resource "null_resource" "run_ansible" {
+    provisioner "local-exec" {
+    inline = [
+      "ansible-playbook -i ~/inventory ~/ansible/playbook.yml ",
+    ]
   }
   depends_on = [
     hetznerdns_record.rancher
   ]
 }
+# resource "null_resource" "wait_for_rancher" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+# while [ "$${resp}" != "pong" ]; do
+#     resp=$(curl -sSk -m 2 "https://$${RANCHER_HOSTNAME}:8443/ping")
+#     echo "Rancher Response: $${resp}"
+#     if [ "$${resp}" != "pong" ]; then
+#       sleep 10
+#     fi
+# done
+# EOF
+
+
+#     environment = {
+#       RANCHER_HOSTNAME = "${local.rancher_hostname}.${local.domain}"
+#     }
+#   }
+#   depends_on = [
+#     hetznerdns_record.rancher
+#   ]
+# }
 
 # resource "rancher2_bootstrap" "admin" {
 #   provider   = rancher2.bootstrap
