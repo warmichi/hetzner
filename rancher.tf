ef0e5d36d31056d01831180cf890e3f3861c7d58
warmichi <michael.uwannah@gmail.com>
2021-08-29 15:44:53 +0000

    Update rancher.tf

--- rancher.tf
+++ rancher.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "run_ansible" {
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${path.root}/inventory ${path.root}/ansible/playbook.yaml"
+    command = "sleep 60 && ansible-playbook -i ${path.root}/inventory ${path.root}/ansible/playbook.yaml"
     environment = {
       ANSIBLE_PRIVATE_KEY_FILE  = "${var.HCLOUD_SSH_RANCHER_PRIVATE_KEY}"
       ANSIBLE_HOST_KEY_CHECKING = "False"
