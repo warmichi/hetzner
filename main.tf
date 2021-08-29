de9d9e5b4deced40a325f9d544b042871e67eef4
Uwannah, Michael <m.uwannah@ambifox.de>
2021-08-29 20:15:01 +0200

    update main

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "hcloud_network_subnet" "network_subnet" {
   ip_range     = local.ip_range
 }
 
-resource "hcloud_server" "rancher" "main" {
+resource "hcloud_server" "rancher" {
   count       = local.rancher_node_count
   name        = local.rancher_hostname
   server_type = local.hetzner_server_type
