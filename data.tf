fa19ceaef9f2a894f6f130d2ecdf4541de932522
Uwannah, Michael <m.uwannah@ambifox.de>
2021-09-21 21:38:32 +0200

    fix identation

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "template_file" "cloud_init" {
   template = file("${path.module}/files/cloud-config.yaml")
   vars = {
-    HCLOUD_SSH_WARMICHI_PUBLIC_KEY = var.HCLOUD_SSH_WARMICHI_PUBLIC_KEY
+    HCLOUD_SSH_WARMICHI_PUBLIC_KEY  = var.HCLOUD_SSH_WARMICHI_PUBLIC_KEY
     HCLOUD_SSH_WARMICHI_PRIVATE_KEY = var.HCLOUD_SSH_WARMICHI_PRIVATE_KEY
   }
 }
