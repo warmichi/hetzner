69e83787f2b699e0a7c78608bb1fed4d59f5a937
Uwannah, Michael <m.uwannah@ambifox.de>
2021-09-25 22:57:43 +0200

    new ansible playbook

---
+++ k8s.tf
@@ -0,0 +1,45 @@
+resource "null_resource" "run_ansible" {
+  triggers = {
+    hcloud_server_ids = join(",", hcloud_server.rancher.*.id)
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 60 && ansible-playbook -i ${path.root}/inventory ${path.root}/ansible/playbook.yaml"
+    environment = {
+      ANSIBLE_PRIVATE_KEY_FILE  = "${var.HCLOUD_SSH_RANCHER_PRIVATE_KEY}"
+      ANSIBLE_HOST_KEY_CHECKING = "False"
+    }
+  }
+  depends_on = [
+    hetznerdns_record.k8s_control_plane,
+    hcloud_server.k8s_control_plane,
+  ]
+}
+
+# resource "null_resource" "wait_for_rancher" {
+#   provisioner "local-exec" {
+#     command = <<EOF
+# while [ "$${resp}" != "pong" ]; do
+#     resp=$(curl -sSk -m 2 "https://$${RANCHER_HOSTNAME}:8443/ping")
+#     echo "Rancher Response: $${resp}"
+#     if [ "$${resp}" != "pong" ]; then
+#       sleep 10
+#     fi
+# done
+# EOF
+
+
+#     environment = {
+#       RANCHER_HOSTNAME = "${local.rancher_hostname}.${local.domain}"
+#     }
+#   }
+#   depends_on = [
+#     hetznerdns_record.rancher
+#   ]
+# }
+
+# resource "rancher2_bootstrap" "admin" {
+#   provider   = rancher2.bootstrap
+#   depends_on = [null_resource.wait_for_rancher]
+#   password   = var.RANCHER_UI_PASSWORD
+# }
--- templates/ansible_skeleton.tpl
+++ templates/ansible_skeleton.tpl
@@ -1,5 +1,5 @@
 [all:vars]
 ansible_ssh_common_args='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
-[rancher]
+[k8s_control_plane]
 ${k8s_control_plane_hosts_def}
