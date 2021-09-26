710c3ee0ad89ae85647afc900ce9b3e138726ce6
Uwannah, Michael <m.uwannah@ambifox.de>
2021-09-26 15:35:30 +0200

    key

--- k8s.tf
+++ k8s.tf
@@ -4,7 +4,7 @@ resource "null_resource" "run_ansible" {
   }
 
   provisioner "local-exec" {
-    command = "sleep 60 && ansible-playbook -i ${path.root}/inventory ${path.root}/ansible/playbook.yaml"
+    command = "chmod 600 ${var.HCLOUD_SSH_ROOT_PRIVATE_KEY} && sleep 60 && ansible-playbook -i ${path.root}/inventory ${path.root}/ansible/playbook.yaml"
     environment = {
       ANSIBLE_PRIVATE_KEY_FILE  = "${var.HCLOUD_SSH_ROOT_PRIVATE_KEY}"
       ANSIBLE_HOST_KEY_CHECKING = "False"
