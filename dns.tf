7c187a1a145821e4af90b31f6e12b2c840af9f75
Uwannah, Michael <m.uwannah@ambifox.de>
2021-09-27 22:48:49 +0200

    dns each

--- dns.tf
+++ dns.tf
@@ -3,11 +3,12 @@ data "hetznerdns_zone" "dns_zone" {
 }
 
 resource "hetznerdns_record" "kube_control_plane" {
-  zone_id = data.hetznerdns_zone.dns_zone.id
-  name    = local.kube_control_plane_hostname
-  value   = hcloud_server.kube_control_plane.*.ipv4_address
-  type    = "A"
-  ttl     = 60
+  zone_id  = data.hetznerdns_zone.dns_zone.id
+  for_each = hcloud_server
+  name     = local.name
+  value    = each.ipv4_address
+  type     = "A"
+  ttl      = 60
 }
 
 # resource "hetznerdns_record" "kube_control_plane_cname" {
