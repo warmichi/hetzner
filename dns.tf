3ece8acba6d7330de13cbcd86b43ad6cff311565
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-16 00:31:16 +0200

    each

--- dns.tf
+++ dns.tf
@@ -5,8 +5,8 @@ data "hetznerdns_zone" "dns_zone" {
 resource "hetznerdns_record" "kube_control_plane" {
   for_each = {for hcloud_server in hcloud_server.kube_control_plane : hcloud_server.name => hcloud}
   zone_id  = data.hetznerdns_zone.dns_zone.id
-  name     = hcloud_server.each.name
-  value    = hcloud_server.each.ipv4_address
+  name     = each.value.name
+  value    = each.value.ipv4_address
   type     = "A"
   ttl      = 60
 }
