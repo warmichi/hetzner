eaa2c2c02a9220c42194074cf7cb55fd026af68a
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-17 20:45:26 +0200

    change output

--- output.tf
+++ output.tf
@@ -1,10 +1,3 @@
 output "env-dynamic-url" {
   value = local.kube_cluster_name
 }
-
-output "yo" {
-  value = data.template_file.ansible_skeleton.rendered
-}
-output "test" {
-  value = data.template_file.ansible_kube_hosts.*.rendered
-}
