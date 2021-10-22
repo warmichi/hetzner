dd4adec38afec457da1d7be23f563a9bfb663648
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-23 01:01:05 +0200

    zipmap

--- ansible_inventory.tf
+++ ansible_inventory.tf
@@ -1,20 +1,21 @@
-data "template_file" "ansible_all_kube_nodes" {
-  count      = local.kube_control_plane_count
-  template   = file("${path.root}/templates/ansible_hosts.tpl")
-  depends_on = [hcloud_server.kube_control_plane]
+# data "template_file" "ansible_all_kube_nodes" {
+#   count      = local.kube_control_plane_count
+#   template   = file("${path.root}/templates/ansible_hosts.tpl")
+#   depends_on = [hcloud_server.kube_control_plane]
 
-  vars = {
-    node_name    = join("", element(hcloud_server.kube_control_plane.*.name, count.index), element(hcloud_server.kube_node.*.name, count.index))
-    ansible_user = local.hetzner_ssh_user
-    ip           = element(hcloud_server.kube_control_plane.*.ipv4_address, count.index)
-  }
-}
+#   vars = {
+#     node_name    = join("", element(hcloud_server.kube_control_plane.*.name, count.index), element(hcloud_server.kube_node.*.name, count.index))
+#     ansible_user = local.hetzner_ssh_user
+#     ip           = element(hcloud_server.kube_control_plane.*.ipv4_address, count.index)
+#   }
+# }
 
 data "template_file" "ansible_skeleton" {
   template = file("${path.root}/templates/ansible_skeleton.tpl")
 
   vars = {
-    ansible_all_kube_nodes_def     = join("", data.template_file.ansible_all_kube_nodes.*.rendered)
+    # ansible_all_kube_nodes_def     = join("", data.template_file.ansible_all_kube_nodes.*.rendered)
+    ansible_all_kube_nodes_def     = zipmap(join("", hcloud_server.kube_control_plane.*.name), join("",hcloud_server.kube_control_plane.*.ipv4_address))
     ansible_kube_control_plane_def = join("", hcloud_server.kube_control_plane.*.name)
     ansible_kube_node_def          = join("", hcloud_server.kube_node.*.name)
     ansible_user                   = local.hetzner_ssh_user
