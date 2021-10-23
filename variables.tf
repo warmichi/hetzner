f7a9b346de600d107aad2f37b71c1aae087cbcef
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-23 02:57:07 +0200

    formatting

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "hcloud_ssh_root_public_key" {
-    type        = string
-    description = "Public root ssh-key for Hetzner"
+  type        = string
+  description = "Public root ssh-key for Hetzner"
 }
 
 variable "hcloud_ssh_root_private_key" {
-    type        = string
-    description = "Private root ssh-key for Hetzner"
+  type        = string
+  description = "Private root ssh-key for Hetzner"
 }
 
 variable "hcloud_ssh_warmichi_public_key" {
-    type        = string
-    description = "Public warmichi ssh-key for Hetzner"
+  type        = string
+  description = "Public warmichi ssh-key for Hetzner"
 }
 
 variable "hcloud_ssh_warmichi_private_key" {
-    type        = string
-    description = "Public warmichi ssh-key for Hetzner"
+  type        = string
+  description = "Public warmichi ssh-key for Hetzner"
 }
