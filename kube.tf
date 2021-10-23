1763a022a8365fb28fed192a0428cb7b19834ebb
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-23 13:24:39 +0200

    fix cluster.yaml path

--- kube.tf
+++ kube.tf
@@ -5,7 +5,7 @@ resource "null_resource" "run_ansible" {
 
   provisioner "local-exec" {
     # command = "chmod 600 ${var.HCLOUD_SSH_ROOT_PRIVATE_KEY} && sleep 60 && ansible-playbook -i ${path.root}/inventory ${path.root}/ansible/playbook.yaml"
-    command = "chmod 600 ${var.hcloud_ssh_root_private_key} && sleep 60 && ansible-playbook -i ${path.root}/inventory cluster.yaml"
+    command = "chmod 600 ${var.hcloud_ssh_root_private_key} && sleep 60 && ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yaml"
     environment = {
       ANSIBLE_PRIVATE_KEY_FILE  = "${var.hcloud_ssh_root_private_key}"
       ANSIBLE_HOST_KEY_CHECKING = "False"
