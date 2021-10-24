85c217af443b25546c409bec32ab58407e55b6ce
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 16:43:54 +0200

    add placement groups

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+resource "hcloud_placement_group" "kube_control_plane" {
+  name = "kube_control_plane"
+  type = "spread"
+}
+
+resource "hcloud_placement_group" "kube_node" {
+  name = "kube_control_plane"
+  type = "spread"
+}
+
 resource "hcloud_ssh_key" "root" {
   name       = var.kube_cluster_name
   public_key = var.hcloud_ssh_root_public_key
@@ -16,12 +26,13 @@ resource "hcloud_network_subnet" "network_subnet" {
 }
 
 resource "hcloud_server" "kube_control_plane" {
-  count       = var.kube_control_plane_count
-  name        = "${var.kube_cluster_name}-control-plane-${count.index + 1}"
-  server_type = var.hetzner_control_plane_type
-  image       = var.hetzner_image
-  location    = var.hetzner_datacenter
-  user_data   = data.template_file.cloud_init.rendered
+  count              = var.kube_control_plane_count
+  name               = "${var.kube_cluster_name}-control-plane-${count.index + 1}"
+  server_type        = var.hetzner_control_plane_type
+  image              = var.hetzner_image
+  location           = var.hetzner_datacenter
+  user_data          = data.template_file.cloud_init.rendered
+  placement_group_id = hcloud_placement_group.kube_control_plane.id
 
   network {
     network_id = hcloud_network.network.id
@@ -41,12 +52,13 @@ resource "hcloud_server" "kube_control_plane" {
 }
 
 resource "hcloud_server" "kube_node" {
-  count       = var.kube_node_count
-  name        = "${var.kube_cluster_name}-node-${count.index + 1}"
-  server_type = var.hetzner_node_server_type
-  image       = var.hetzner_image
-  location    = var.hetzner_datacenter
-  user_data   = data.template_file.cloud_init.rendered
+  count              = var.kube_node_count
+  name               = "${var.kube_cluster_name}-node-${count.index + 1}"
+  server_type        = var.hetzner_node_server_type
+  image              = var.hetzner_image
+  location           = var.hetzner_datacenter
+  user_data          = data.template_file.cloud_init.rendered
+  placement_group_id = hcloud_placement_group.kube_node.id
 
   network {
     network_id = hcloud_network.network.id
