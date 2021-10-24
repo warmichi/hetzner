7cb6ba917faa6a9b52c889cde1084b4dc22c0357
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 11:12:37 +0200

    new control plane server type

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
   kube_worker_count        = 2
 
   hetzner_worker_server_type = "cx21"
-  hetzner_control_plane_type = "cx21"
+  hetzner_control_plane_type = "cpx11"
   hetzner_image              = "ubuntu-20.04"
   hetzner_datacenter         = "nbg1"
   hetzner_ssh_user           = "root"
