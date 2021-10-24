fae0190d0c449bf3fa3a97b3a5024c0b71a51f91
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 15:48:40 +0200

    add node dns

--- dns.tf
+++ dns.tf
@@ -11,6 +11,15 @@ resource "hetznerdns_record" "kube_control_plane" {
   ttl     = 60
 }
 
+resource "hetznerdns_record" "kube_node" {
+  count   = var.kube_node
+  zone_id = data.hetznerdns_zone.dns_zone.id
+  name    = hcloud_server.kube_node[count.index].name
+  value   = hcloud_server.kube_node[count.index].ipv4_address
+  type    = "A"
+  ttl     = 60
+}
+
 # resource "hetznerdns_record" "kube_control_plane_cname" {
 #   zone_id = data.hetznerdns_zone.dns_zone.id
 #   name    = var.kube_cluster_name
