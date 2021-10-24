f3f6d0555327b8cd450412db88093275d013aab9
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 11:14:43 +0200

    clear provider file

--- providers.tf
+++ providers.tf
@@ -3,17 +3,3 @@ provider "hcloud" {
 
 provider "hetznerdns" {
 }
-
-# provider "rancher2" {
-#   alias     = "bootstrap"
-#   api_url   = local.rancher_api_url
-#   bootstrap = true
-#   insecure  = true
-# }
-
-# provider "rancher2" {
-#   alias     = "admin"
-#   api_url   = rancher2_bootstrap.admin.url
-#   token_key = rancher2_bootstrap.admin.token
-#   insecure  = true
-# }
