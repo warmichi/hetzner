cbdec37149138ab4ca9e28319eeb0ca376eac12f
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 16:00:36 +0200

    scaling

--- kube.tf
+++ kube.tf
@@ -1,14 +1,14 @@
 resource "null_resource" "run_ansible" {
   triggers = {
-    hcloud_server_ids = join(",", hcloud_server.kube_control_plane.*.id, hcloud_server.kube_node.*.id)
+    hcloud_server_ids = join(",", hcloud_server.kube_control_plane.*.id, hcloud_server.kube_node.*.id),
+    kube_version      = var.kube_version
   }
 
   provisioner "local-exec" {
     command = <<EOT
-      chmod 600 ${var.hcloud_ssh_root_private_key} && sleep 60 && ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml
-      chmod 600 ${var.hcloud_ssh_root_private_key} && sleep 60 && ansible-playbook -i ${path.root}/inventory /kubespray/scale.yml
-      if test -f "destroyed_nodes"; then
-        ansible-playbook -i ${path.root}/inventory /kubespray/remove-node.yml -b -v --extra-vars "node=$(cat destroyed_nodes | sed 's/^\|$//g'|paste -sd, - )"
+      sleep 60 && ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml -e kube_version=${var.kube_version}
+      if [ "$SCALE_CLUSTER" = true ] ; then
+        ansible-playbook -i ${path.root}/inventory /kubespray/scale.yml
       fi
     EOT
 
@@ -17,8 +17,11 @@ resource "null_resource" "run_ansible" {
       ANSIBLE_HOST_KEY_CHECKING = "False"
     }
   }
+
   depends_on = [
     hetznerdns_record.kube_control_plane,
     hcloud_server.kube_control_plane,
+    hetznerdns_record.kube_node,
+    hcloud_server.kube_node
   ]
 }
