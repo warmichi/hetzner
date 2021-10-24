2beb5a662c21350f1fd27d832aee2365d4c54d15
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 16:17:24 +0200

    fix ssh-key

--- kube.tf
+++ kube.tf
@@ -6,9 +6,23 @@ resource "null_resource" "run_ansible" {
 
   provisioner "local-exec" {
     command = <<EOT
-      sleep 60 && ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml -e kube_version=${var.kube_version}
+      chmod 600 ${var.hcloud_ssh_root_private_key}
+      sleep 60
+
+      # Bootstrap or scale cluster when enviroment variable is set 
       if [ "$SCALE_CLUSTER" = true ] ; then
         ansible-playbook -i ${path.root}/inventory /kubespray/scale.yml
+      then
+        ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml -e kube_version=${var.kube_version}
+      fi
+
+      # Graceful Upgrade Cluster when enviroment variable is set
+      if [ "$UPGRADE_CLUSTER" = true ] ; then
+        ansible-playbook -i ${path.root}/inventory /kubespray/upgrade-cluster.yml -e kube_version=${var.kube_version}
+
+      # Destroy nodes
+      if test -f "destroyed_nodes"; then
+        ansible-playbook -i ${path.root}/inventory /kubespray/remove-node.yml -b -v --extra-vars reset_nodes=false "node=$(cat destroyed_nodes | sed 's/^\|$//g'|paste -sd, - )"
       fi
     EOT
 
