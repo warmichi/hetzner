f81d4af5873047b7bfb8ead7bdfe13e1d9807939
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-24 16:43:03 +0200

    new hcloud version

--- versions.tf
+++ versions.tf
@@ -2,15 +2,11 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.28.1"
+      version = "1.31.1"
     }
     hetznerdns = {
       source  = "timohirt/hetznerdns"
       version = "1.1.1"
-    }
-    rancher2 = {
-      source  = "rancher/rancher2"
-      version = "1.16.0"
     }
   }
 }
