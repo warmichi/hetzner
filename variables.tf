ebb5d91d761a133ac360e7f004f8e73ef87f13f8
Uwannah, Michael <m.uwannah@ambifox.de>
2021-10-30 17:13:47 +0200

    more workers

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "kube_control_plane_count" {
 variable "kube_node_count" {
   type        = number
   description = "Number of worker nodes"
-  default     = 0
+  default     = 2
 }
 
 variable "hetzner_control_plane_type" {
