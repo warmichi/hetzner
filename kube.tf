b6265baf30e91b13bd6c6fa8b3c4c84260697eb8
Uwannah, Michael <m.uwannah@ambifox.de>
2022-04-16 21:12:55 +0200

    add extra-vars

--- kube.tf
+++ kube.tf
@@ -11,9 +11,9 @@ resource "null_resource" "run_ansible" {
       # Bootstrap cluster when enviroment variable is set 
       if [ "$BOOTSTRAP_KUBE_CLUSTER" = true ] ; then
         echo "Bootstrap Kube Cluster ..."
-        ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml --extra-vars '${jsonencode(var.kube_cluster_variables)}' 'hcloud_api_token=$TF_VAR_hcloud_token'
+        ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml -e '${jsonencode(var.kube_cluster_variables)}' -e 'hcloud_api_token=$TF_VAR_hcloud_token'
       fi
-      sleep 360000
+      
       # Scale cluster when enviroment variable is set 
       if [ "$SCALE_KUBE_CLUSTER" = true ] ; then
         echo "Scale Kube Cluster ..."
