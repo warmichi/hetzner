f98aa2f7bae0624a55463c81e54d7c7179448cca
Uwannah, Michael <m.uwannah@ambifox.de>
2022-04-21 23:03:27 +0200

    no sleep

--- kube.tf
+++ kube.tf
@@ -12,7 +12,6 @@ resource "null_resource" "run_ansible" {
       if [ "$BOOTSTRAP_KUBE_CLUSTER" = true ] ; then
         echo "Bootstrap Kube Cluster ..."
         ansible-playbook -i ${path.root}/inventory /kubespray/cluster.yml -e '${jsonencode(var.kube_cluster_variables)}' -e hcloud_api_token=$TF_VAR_hcloud_token
-        sleep 1000000
       fi
 
       # Scale cluster when enviroment variable is set 
