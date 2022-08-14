4f894f5e48b8e000f5f49a39108b8de9c4ce2d82
Uwannah, Michael <m.uwannah@ambifox.de>
2022-08-14 18:06:10 +0200

    format

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   kubespray_config = merge(
     var.kubespray_kube_config,
-    {network_id = var.kube_cluster_name},
+    { network_id = var.kube_cluster_name },
     var.kubespray_argocd_config,
     var.kubespray_ingress_config,
-    {external_hcloud_cloud = merge(var.kubespray_cloud_provider_config["external_hcloud_cloud"], {hcloud_api_token = var.hcloud_token})} # inject hcloud token variable
+    { external_hcloud_cloud = merge(var.kubespray_cloud_provider_config["external_hcloud_cloud"], {hcloud_api_token = var.hcloud_token}) } # inject hcloud token variable
   )
 }
