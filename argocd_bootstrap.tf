fe4be04ea45b538896350b98c5b9f882697b31e4
warmichi <michael.uwannah@gmail.com>
2022-08-30 19:10:10 +0300

    fix fmt

--- argocd_bootstrap.tf
+++ argocd_bootstrap.tf
@@ -1,7 +1,7 @@
 data "template_file" "argocd_bootstrap_tmpl" {
   template = file("${path.root}/templates/argocd_bootstrap.tpl")
 
-  vars = { argocd_bootstrap_repo_url  = var.argocd_bootstrap_repo_url }
+  vars = { argocd_bootstrap_repo_url = var.argocd_bootstrap_repo_url }
 }
 
 resource "local_file" "argocd_bootstrap_task" {
