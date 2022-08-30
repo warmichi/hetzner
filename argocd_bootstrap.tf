339f87587f0e98062007e3b010895fc54a3705e8
warmichi <michael.uwannah@gmail.com>
2022-08-30 19:07:06 +0300

    fix fmt

--- argocd_bootstrap.tf
+++ argocd_bootstrap.tf
@@ -1,13 +1,12 @@
 data "template_file" "argocd_bootstrap_tmpl" {
   template = file("${path.root}/templates/argocd_bootstrap.tpl")
 
-  vars = { argocd_bootstrap_repo_url  = var.argocd_bootstrap_repo_url
-  }
+  vars = { argocd_bootstrap_repo_url  = var.argocd_bootstrap_repo_url }
 }
 
 resource "local_file" "argocd_bootstrap_task" {
   depends_on = [data.template_file.argocd_bootstrap_tmpl]
-  
+
   content  = data.template_file.argocd_bootstrap_tmpl.rendered
   filename = "${path.root}/kubespray/roles/kubernetes-apps/argocd/tasks/argocd_bootstrap.yml"
 }
