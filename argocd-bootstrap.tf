7256221121ab9815b214589bfa2255b43de064cb
Uwannah, Michael <m.uwannah@ambifox.de>
2022-10-02 21:12:29 +0200

    fix fmt

--- argocd-bootstrap.tf
+++ argocd-bootstrap.tf
@@ -2,7 +2,6 @@
 # Patch for Argocd Repo Server with Vault Sidecar Containers
 data "template_file" "argocd_repo_server_tmpl" {
   template = file("${path.root}/templates/argocd-repo-server.yml.tpl")
-
   vars = {
     argocd_version = var.kubespray_argocd_config["argocd_version"]
   }
@@ -18,7 +17,7 @@ resource "local_file" "argocd_repo_server_tmpl" {
 # ConfigMap for Argocd Repo Server with Vault Sidecar Containers
 data "template_file" "argocd_cmp_plugin" {
   template = file("${path.root}/templates/argocd-cmp-plugin.yml.tpl")
-  
+
   vars = {
     argocd_namespace = var.kubespray_argocd_config["argocd_namespace"]
   }
