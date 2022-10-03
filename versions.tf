9a7d93034a0bcad736eb6dbfe2f9b3840741dccf
Uwannah, Michael <m.uwannah@ambifox.de>
2022-10-03 11:52:06 +0200

    update terraform providers

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "1.31.1"
+      version = "1.31.2"
     }
     hetznerdns = {
       source  = "timohirt/hetznerdns"
-      version = "1.1.1"
+      version = "2.1.0"
     }
   }
 }
