32c7af072ba387326259381dca00a52fd6f4ac5c
warmichi <michael.uwannah@gmail.com>
2023-08-02 23:56:49 +0200

    some variable to uppercase

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "template_file" "cloud_init" {
   template = file("${path.module}/files/cloud-config.yaml")
   vars = {
-    hcloud_ssh_warmichi_public_key = var.hcloud_ssh_warmichi_public_key
+    hcloud_ssh_warmichi_public_key = var.HCLOUD_SSH_WARMICHI_PUBLIC_KEY
   }
 }
