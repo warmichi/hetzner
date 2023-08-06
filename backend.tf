b10a9be1d7f719f4641ee4b18f4ee6605080cacc
warmichi <michael.uwannah@gmail.com>
2023-08-06 15:22:53 +0200

    change backend to tfc

--- backend.tf
+++ backend.tf
@@ -1,19 +1,10 @@
-# terraform {
-#   backend "remote" {
-#     hostname     = "app.terraform.io"
-#     organization = "warmichi"
-
-#     workspaces {
-#       name = "hetzner"
-#     }
-#   }
-# }
-
 terraform {
-  backend "s3" {
-    bucket     = "kube-1-terraform-state-prod"
-    key        = "terraform.tfstate"
-    endpoint   = "s3.uwannah.com" 
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "warmichi"
+
+    workspaces {
+      name = "hetzner"
+    }
   }
 }
-
