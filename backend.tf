0020e2f0b9f69613d8bf031a3a0750866384b398
warmichi <michael.uwannah@gmail.com>
2023-08-06 14:48:37 +0200

    new tf backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,21 @@
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "warmichi"
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "warmichi"
 
-    workspaces {
-      name = "hetzner"
-    }
+#     workspaces {
+#       name = "hetzner"
+#     }
+#   }
+# }
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket   = "kube-1-terraform-state-prod"
+    key      = "terraform.tfstate"
+    endpoint = "s3.uwannah.com" 
+    access_key = var.ACCESS_KEY
+    secret_key = var.SECRET_ACCESS_KEY
   }
 }
