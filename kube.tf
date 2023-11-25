d9c1d1d3bbd4a903d8bc12fdf1816cfe3bcb7628
warmichi <michael.uwannah@gmail.com>
2023-11-25 22:15:20 +0100

    fix ansible user

--- kube.tf
+++ kube.tf
@@ -19,7 +19,7 @@ resource "null_resource" "run_ansible" {
       echo $BOOTSTRAP_KUBE_CLUSTER
       if [ "$BOOTSTRAP_KUBE_CLUSTER" = true ] ; then
         echo "Bootstrap Kube Cluster ..."
-        ansible-playbook -i /kubespray/inventory/kube-1/inventory.ini /kubespray/cluster.yml -e $KUBESPRAY_CONFIG 
+        ansible-playbook -i /kubespray/inventory/kube-1/inventory.ini /kubespray/cluster.yml -e $KUBESPRAY_CONFIG -b
       fi
 
       # Scale cluster when enviroment variable is set 
@@ -38,6 +38,7 @@ resource "null_resource" "run_ansible" {
     environment = {
       ANSIBLE_PRIVATE_KEY_FILE  = "${var.HCLOUD_SSH_WARMICHI_PRIVATE_KEY}"
       ANSIBLE_HOST_KEY_CHECKING = "False"
+      ANSIBLE_REMOTE_USER = "warmichi"
       ANSIBLE_SSH_ARGS = "-o UserKnownHostsFile=/dev/null"
       KUBESPRAY_CONFIG = "${jsonencode(local.kubespray_config)}"
     }
